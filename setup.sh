b2ba127c327f19577005d6047282448e8978e840
Enrique Neyra <94760483+EnriqueNeyra@users.noreply.github.com>
2025-04-02 20:14:42 -0700

    update setup

--- setup.sh
+++ setup.sh
@@ -8,10 +8,6 @@ sudo sed -i 's/^dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
 sudo sed -i 's/^#dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
 sudo raspi-config nonint do_i2c 0
 
-# echo "Installing required python package(s)..."
-# sudo apt-get install python3-qrcode
-# sudo apt-get install python3-flask
-
 echo "Setting up python script epaper service..."
 SERVICE_NAME="epaper.service"
 SERVICE_PATH="/etc/systemd/system/${SERVICE_NAME}"
@@ -23,7 +19,7 @@ Description=ePaper Display Service
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 $(pwd)/main.py
+ExecStart=/usr/bin/python3 $(pwd)/sd_monitor.py
 WorkingDirectory=$(pwd)
 Restart=always
 User=$CURRENT_USER
